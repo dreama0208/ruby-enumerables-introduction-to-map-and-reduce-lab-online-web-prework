18106dfd5671279ab924046f2734e4ab022519e0
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-08 22:32:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,7 +35,7 @@ def reduce_to_all_true (source_array)
 return true
 end
 
-def reduce_to_any_tru (source_array)
+def reduce_to_any_true (source_array)
   i = 0
   while i < source_array.length do
     if source_array[i] == true
