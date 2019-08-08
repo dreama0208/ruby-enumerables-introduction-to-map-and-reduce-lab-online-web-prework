925728728c9c8dfcc54929f5c77084d708f0d5fa
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-08 22:28:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,36 @@
 # My Code here....
+
+def map_to_negativize (source_array)
+  source_array.map! { |e| -e }
+  source_array
+end
+
+def map_to_no_change (source_array)
+  source_array
+end
+
+def map_to_double (source_array)
+  source_array.map! { |e| e*2 }
+  source_array
+end
+
+def map_to_square (source_array)
+  source_array.map! { |e| e**2 }
+  source_array
+end
+
+def reduce_to_total (source_array, starting_point = 0)
+      source_array.map { |e| starting_point += e }
+      starting_point
+end
+
+def reduce_to_all_true (source_array)
+  i = 0
+  while i < source_array.length { |i|
+    if source_array[i] == false
+      return false
+    end
+  }
+end
+return true
+end
