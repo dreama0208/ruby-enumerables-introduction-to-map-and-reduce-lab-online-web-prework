b61a7bfaeba828f211731d97494bbb1f55e07cb0
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-08 22:29:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -26,11 +26,11 @@ end
 
 def reduce_to_all_true (source_array)
   i = 0
-  while i < source_array.length { |i|
+  while i < source_array.length
     if source_array[i] == false
       return false
     end
-  }
-end
+    i +=1
+  end
 return true
 end
